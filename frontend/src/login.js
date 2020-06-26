8a977782e6867196d728bdc5b2bb6ca2e5251ad7
Nilesh Kumar Gupta <f20180233@goa.bits-pilani.ac.in>
2020-06-26 21:50:11 +0530

    making it into a single file

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,5 +1,18 @@
+const listContainer = document.querySelector("#sortablelist");
+const listItemTemplate = document.querySelector("#listItemTemplate");
+const saveButton = document.querySelector("#saveButton");
+const logoutButton = document.querySelector("#logoutButton");
+M.AutoInit();
+
+// sortable setting:w
+new Sortable(sortablelist, {
+  animation: 150,
+  ghostClass: "sortable-ghost",
+});
+
 let details = {};
 
+// the signin function
 document
   .querySelector(".g-signin2")
   .addEventListener("data-onsuccess", (googleUser) => {
@@ -12,4 +25,69 @@ document
     // id for authentication
     localStorage["idToken"] = googleUser.getAuthResponse().id_token;
     console.log(details);
+
+    // make the logout option and save buttons visible
+    document.querySelector("#nav-mobile").style.display = "block";
+    // make the pref area visible
+    document.querySelector("#pref").style.display = "block";
+    // make the sigin invisible
+    document.querySelector("#login").style.display = "none";
+    // populate preference list
+    axios
+      .get("https://ttselect.herokuapp.com/prefList", {
+        params: {
+          id: bitsId,
+        },
+      })
+      .then((res) => {
+        // populate the item list
+        let group = res.data["group"];
+        let prefList = res.data["prefList"];
+
+        prefList.forEach((element) => {
+          let newListItem = listItemTemplate.cloneNode(true);
+          newListItem.style.display = "block";
+          newListItem.querySelector(".ttname").innerText =
+            group + " " + element;
+          listContainer.appendChild(newListItem);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
+
+// savebutton function
+
+saveButton.addEventListener("click", () => {
+  let finalPrefList = [];
+  Array.from(listContainer.childNodes).forEach((element) => {
+    if (
+      element.innerText &&
+      element.innerText.length &&
+      element.innerText.length < 5 // to remove the template element
+    ) {
+      finalPrefList.push(element.innerText.split(" ")[1]);
+    }
+  });
+
+  axios
+    .put("http://ttselect.herokuapp.com/preflist", {
+      id: bitsId,
+      prefList: finalPrefList,
+    })
+    .then((res) => {
+      M.toast({ html: "successfully saved" });
+      setTimeout(() => location.reload(), 2000);
+    })
+    .catch((err) => {
+      M.toast({ html: "some error occured, please try again" });
+      console.log(err);
+    });
+});
+
+// logout button function
+logoutButton.addEventListener("click", () => {
+  // implement google signout
+  // redirect to index.html
+});
